18eb7e861316579bd3df5e99f1cffaa2e7923fa6
Ken Ting <kaleungting@gmail.com>
2020-05-08 11:43:50 -0400

    Fix deleteTask

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./styles/index.scss";
 import "./styles/form.scss";
 import "./styles/task.scss";
 import { increaseBar, updateCompetency } from "./scripts/progress";
-import { deleteTask } from "./scripts/util.js";
 
 const taskInput = document.querySelector(".task-input");
 const descriptionInput = document.querySelector(".description-input");
@@ -166,8 +165,6 @@ function addGoal(event) {
   goal.classList.add("goal");
   goalInput.classList.add("hide");
   goalContainer.appendChild(goal);
-
-  // move();
 }
 
 function formShow() {
@@ -179,6 +176,34 @@ function formShow() {
   } else {
     formContainer.style.display = "none";
   }
+}
+
+export function deleteTask(e) {
+  const target = e.currentTarget;
+  const task = target.parentElement;
+  const totalHours = document.querySelector(".total-hours");
+
+  let newTasks = JSON.parse(localStorage.getItem("tasks")).reverse();
+  // let idx = parseInt(task.id) - 1;
+  let idx = Array.from(task.parentNode.children).indexOf(task);
+  task.style.backgroundColor = "#ea7317";
+  setTimeout(function () {
+    task.remove();
+  }, 1000);
+  let subtract = parseInt(
+    JSON.parse(localStorage.getItem("tasks"))[newTasks.length - idx - 1][
+      "log-hours"
+    ]
+  );
+  let sumHours = parseInt(localStorage.getItem("sum"));
+  let newHours = sumHours - subtract;
+  totalHours.innerText = 10000 - newHours;
+  localStorage.setItem("sum", newHours);
+  newTasks.splice(idx, 1);
+  newTasks.reverse();
+  console.log(newTasks);
+  localStorage.setItem("tasks", JSON.stringify(newTasks));
+  //   location.reload();
 }
 
 increaseBar();
--- src/scripts/infinite_scrolling.js
+++ src/scripts/infinite_scrolling.js
@@ -1,5 +1,3 @@
-import { deleteTask } from "./util.js";
-
 let savedTasks = localStorage.getItem("tasks")
   ? JSON.parse(localStorage.getItem("tasks"))
   : [];
@@ -134,35 +132,35 @@ if (savedTasks.length > 0) {
 //   }
 // }
 
-// function deleteTask(e) {
-//   const target = e.currentTarget;
-//   const task = target.parentElement;
-//   const totalHours = document.querySelector(".total-hours");
+function deleteTask(e) {
+  const target = e.currentTarget;
+  const task = target.parentElement;
+  const totalHours = document.querySelector(".total-hours");
 
-//   let newTasks = JSON.parse(localStorage.getItem("tasks")).reverse();
-//   // let idx = parseInt(task.id) - 1;
-//   let idx = Array.from(task.parentNode.children).indexOf(task);
-//   task.style.backgroundColor = "#ea7317";
-//   setTimeout(function () {
-//     task.remove();
-//   }, 1000);
-//   let subtract = parseInt(
-//     JSON.parse(localStorage.getItem("tasks"))[newTasks.length - idx - 1][
-//       "log-hours"
-//     ]
-//   );
-//   let sumHours = parseInt(localStorage.getItem("sum"));
-//   let newHours = sumHours - subtract;
-//   totalHours.innerText = 10000 - newHours;
-//   localStorage.setItem("sum", newHours);
-//   newTasks.splice(idx, 1);
-//   newTasks.reverse();
-//   console.log(newTasks);
-//   localStorage.setItem("tasks", JSON.stringify(newTasks));
-//   //   location.reload();
-//   increaseBar();
-//   updateCompetency();
-// }
+  let newTasks = JSON.parse(localStorage.getItem("tasks")).reverse();
+  // let idx = parseInt(task.id) - 1;
+  let idx = Array.from(task.parentNode.children).indexOf(task);
+  task.style.backgroundColor = "#ea7317";
+  setTimeout(function () {
+    task.remove();
+  }, 1000);
+  let subtract = parseInt(
+    JSON.parse(localStorage.getItem("tasks"))[newTasks.length - idx - 1][
+      "log-hours"
+    ]
+  );
+  let sumHours = parseInt(localStorage.getItem("sum"));
+  let newHours = sumHours - subtract;
+  totalHours.innerText = 10000 - newHours;
+  localStorage.setItem("sum", newHours);
+  newTasks.splice(idx, 1);
+  newTasks.reverse();
+  console.log(newTasks);
+  localStorage.setItem("tasks", JSON.stringify(newTasks));
+  //   location.reload();
+  increaseBar();
+  updateCompetency();
+}
 
 window.addEventListener("DOMContentLoaded", () => {});
 const sumHours = parseInt(localStorage.getItem("sum"));
