7336e98e4563e104691bda4a431e767b8e73b969
Ken Ting <kaleungting@gmail.com>
2020-05-08 20:11:52 -0400

    N fix

--- src/index.js
+++ src/index.js
@@ -210,3 +210,52 @@ increaseBar();
 updateCompetency();
 
 //shortcuts
+
+document.addEventListener("keydown", formShortcut, false);
+document.addEventListener("keydown", downShortcut, false);
+document.addEventListener("keydown", upShortcut, false);
+
+function formShortcut(e) {
+  const taskInput = document.querySelector(".task-input");
+  const taskFocus = document.activeElement === taskInput;
+  const descriptionInput = document.querySelector(".description-input");
+  const descriptionFocus = document.activeElement === descriptionInput;
+  // debugger;
+  if (e.key === "n" && !taskFocus && !descriptionFocus) {
+    formShow();
+    e.preventDefault();
+    taskInput.focus();
+  } else if (
+    e.key === "Escape" &&
+    formContainer.style.display !== "none" &&
+    formContainer.style.display !== ""
+  ) {
+    formShow();
+  }
+}
+
+function downShortcut(e) {
+  if (
+    e.key === "d" &&
+    (formContainer.style.display === "none" ||
+      formContainer.style.display === "")
+  ) {
+    window.scroll({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  }
+}
+
+function upShortcut(e) {
+  if (
+    e.key === "u" &&
+    (formContainer.style.display === "none" ||
+      formContainer.style.display === "")
+  ) {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+}
